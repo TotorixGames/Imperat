61de9a227dc53e494e3cae0956bf6d4c5635cfe9
einjojo Johannes <johannes@einjojo.it>
2026-05-08 19:59:37 +0200

    chore(build): update base version and group name

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     kotlin("jvm") version "2.3.0" apply false
 }
 
-val baseVersion = "3.6.0"
+val baseVersion = "3.8.0"
 val releaseSnapshots = true
 val isSnapshot = System.getenv("SNAPSHOT_BUILD") == "true"
 val rootJavaVersion = 21
@@ -26,7 +26,7 @@ allprojects {
         options.encoding = "UTF-8"
     }
 
-    group = "studio.mevera"
+    group = "net.totorix"
     version = baseVersion
 
     if (isSnapshot && releaseSnapshots) {
